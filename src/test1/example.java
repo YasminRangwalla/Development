e884e9314d46d33b93503b694e4dd3ffa2c7fde9
yasmin <yasmin@Yasmin.lan>
2018-04-13 12:33:57 +0400

    First commit

---
+++ src/test1/example.java
@@ -0,0 +1,16 @@
+package test1;
+
+public class example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=100;
+		int c;
+		if(a==100){
+			c=100;
+		}
+		System.out.println(c);
+
+	}
+
+}
---
+++ src/test1/test1.java
@@ -0,0 +1,22 @@
+package test1;
+
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		* // anteater
+		*/
+		// bear
+		// // cat
+		
+		// /* dog */
+		/* elephant */
+		/*
+		* /* ferret */
+		*/
+		System.out.println("hello");
+	}
+
+}
